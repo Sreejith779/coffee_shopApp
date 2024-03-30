f3a2fe963e3c335909bb251df8f7ea7038981ad0
Sreejith779 <sreejithhkm@gmail.com>
2024-03-30 23:44:49 +0530

    initial commit

---
+++ lib/features/onBoardingPage/bloc/on_boarding_event.dart
@@ -0,0 +1,4 @@
+part of 'on_boarding_bloc.dart';
+
+@immutable
+sealed class OnBoardingEvent {}
---
+++ lib/features/onBoardingPage/bloc/on_boarding_state.dart
@@ -0,0 +1,6 @@
+part of 'on_boarding_bloc.dart';
+
+@immutable
+sealed class OnBoardingState {}
+
+final class OnBoardingInitial extends OnBoardingState {}
